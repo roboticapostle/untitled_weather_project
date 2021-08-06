bfeca9111341a486650ce561a036cc038eea6830
Kyle Reimer <k.m.reimer@icloud.com>
2021-08-06 09:43:58 -0400

    Added gridpoint query code and main function for testing to query.py
    Added docker file.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3
+
+RUN pip install --upgrade pip && \
+    pip install requests
+
+WORKDIR /usr/src/app
+
+# In order to launch our python code, we must import it into our image.
+# We use the keyword 'COPY' to do that.
+# The first parameter 'main.py' is the name of the file on the host.
+# The second parameter '/' is the path where to put the file on the image.
+# Here we put the file at the image root folder.
+COPY src/query.py /
+
+# We need to define the command to launch when we are going to run the image.
+# We use the keyword 'CMD' to do that.
+# The following command will execute "python ./main.py".
+CMD [ "python", "./query.py" ]
--- src/query.py
+++ src/query.py
@@ -1,17 +1,61 @@
+import requests
+import json
+
 class Forcast:
     def __init__(self, lat, long):
     	self.lat = lat
     	self.long = long
     	self.gridX = self.gridY = None
     	self.queryURL = "https://api.weather.gov/"
-    
-    def query(self):
   
-    def parseQuery(self):
+    #def parseQuery(self):
+
     
-    def getQueryResult(self):
+    #def getQueryResult(self):
+
     
     def convertLatLongToGridPoints(self):
-    	if self.gridX and self.gridY is None:
-    		
-    		
+        header = 'User-Agent: (project_lapis_manalis, roboticapostle@gmail.com)'
+        url = 'https://api.weather.gov/points/' + self.lat + ',' + self.long
+        gridRequest = requests.get(url, headers = {'User-Agent': '(project_lapis_manalis, roboticapostle@gmail.com)'})
+        if(gridRequest.status_code == 200):
+            requestRet = json.loads(gridRequest.text)
+            self.gridX = requestRet["properties"]["gridX"]
+            self.gridY = requestRet["properties"]["gridY"]
+        else:
+            print('Request for lat and long returned error code: ' + str(gridRequest.status_code))
+
+    
+    def query(self):
+        if self.gridX == None and self.gridY == None:
+           self.convertLatLongToGridPoints()
+
+# Testing function for this class
+if __name__ == "__main__":
+    isEntryDone = False
+    while isEntryDone == False:
+        print("Enter your latitude")
+        lat = input()
+
+        print("Enter your longitude")
+        long = input()
+
+        print("You entered: " + str(lat) + " " + str (long) + " ")
+        print("Is this correct? (y/n)")
+
+        isSelectionMade = False
+
+        while isSelectionMade == False:
+            done = input()
+
+            if done == "n":
+                isSelectionMade = True
+                isEntryDone = False
+            elif done == "y":
+                isSelectionMade = True
+                isEntryDone = True
+            else:
+                print("Invalid selection: enter y or n")
+                isSelectionMade = False
+        forcast = Forcast(lat,long)
+        forcast.query()
