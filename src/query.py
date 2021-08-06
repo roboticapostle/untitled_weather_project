4908c1df760d4ca1d400ce9b12763bee059a8119
Kyle Reimer <k.m.reimer@icloud.com>
2021-08-06 07:22:53 -0400

    Adding source files

---
+++ src/query.py
@@ -0,0 +1,17 @@
+class Forcast:
+    def __init__(self, lat, long):
+    	self.lat = lat
+    	self.long = long
+    	self.gridX = self.gridY = None
+    	self.queryURL = "https://api.weather.gov/"
+    
+    def query(self):
+  
+    def parseQuery(self):
+    
+    def getQueryResult(self):
+    
+    def convertLatLongToGridPoints(self):
+    	if self.gridX and self.gridY is None:
+    		
+    		
